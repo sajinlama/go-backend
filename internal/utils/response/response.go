b31bfa27dba7111dc0af7a60c1e394808bdc0db7
sajinlama <tamangsajin99@gmail.com>
2026-09-05 20:04:14 +0545

    add validation

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -5,10 +5,27 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string
+	error  string
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
 func Writejson(w http.ResponseWriter, status int, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(data)
+
+}
+
+func GeneralError(err error) Response {
+	return Response{
+		Status: StatusError,
+	}
 
 }
